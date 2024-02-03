c2f3de3fdca973497a4e027221da8b0b72869741
otis11 <otis11>
2024-02-03 15:35:37 +0100

    improve: use the same code for mariadb and mysql as its the same connector

--- src/modules/mariadb/getDatabaseTreeChildren.ts
+++ src/modules/mariadb/getDatabaseTreeChildren.ts
@@ -1,59 +1,8 @@
 import { TreeItem } from "vscode"
-import { DatabaseTreeItem, FolderTreeItem, PoolConnectionTreeItem, TableTreeItem, getPoolConnection } from "../core"
-import { MariaDBConnectionPool } from "./MariaDBConnectionPool"
+import { getDatabaseTreeChildren as getMysqlChildren } from "../mysql/getDatabaseTreeChildren"
 
 export async function getDatabaseTreeChildren(item: TreeItem): Promise<TreeItem[]> {
-    if (item instanceof PoolConnectionTreeItem) {
-        // root
-        const connection = getPoolConnection(item.connectionConfig) as MariaDBConnectionPool
-        const { rows, fields } = await connection.fetchDatabases()
-        return rows.map(row => new DatabaseTreeItem(
-            {
-                ...item.connectionConfig,
-                connection: {
-                    ...item.connectionConfig.connection,
-                    database: row[fields[0].name]?.toString() || '',
-                }
-            },
-        ))
-    }
-
-    else if (item instanceof DatabaseTreeItem) {
-        const connection = getPoolConnection(item.connectionConfig) as MariaDBConnectionPool
-        const { rows } = await connection.fetchDatabaseStats()
-        return [
-            new FolderTreeItem(
-                "tables",
-                "tableFolder",
-                item.connectionConfig,
-                rows[0].TotalTables?.toString()),
-            new FolderTreeItem(
-                "views",
-                "viewFolder",
-                item.connectionConfig,
-                rows[0].TotalViews?.toString()),
-        ]
-    }
-
-    else if (item instanceof FolderTreeItem && item.contextValue === 'tableFolder') {
-        const connection = getPoolConnection(item.connectionConfig) as MariaDBConnectionPool
-        const { rows, fields } = await connection.fetchTables()
-        return rows.map(row => new TableTreeItem(
-            item.connectionConfig,
-            row[fields[0].name]?.toString() || ''
-        ))
-    }
-
-    else if (item instanceof FolderTreeItem && item.contextValue === 'viewFolder') {
-        const connection = getPoolConnection(item.connectionConfig) as MariaDBConnectionPool
-        const { rows, fields } = await connection.fetchViews()
-        return rows.map(row => new TableTreeItem(
-            item.connectionConfig,
-            row[fields[0].name]?.toString() || ''
-        ))
-    }
-
-    return []
+    return getMysqlChildren(item)
 }
 
 
